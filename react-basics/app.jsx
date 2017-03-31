320d2a947ac24fdb148bb88b267b0ee131542113
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2017-03-31 15:24:05 -0400

    Integrated PropTypes for title property and added a default value

--- react-basics/app.jsx
+++ react-basics/app.jsx
@@ -1,8 +1,8 @@
-function Application() {
+function Application(props) {
   return (
-    <div className="application">
+    <div className="scoreboard">
       <div className="header">
-        <h1>Scoreboard</h1>
+        <h1>{props.title}</h1>
       </div>
 
       <div className="players">
@@ -19,8 +19,47 @@ function Application() {
           </div>
         </div>
       </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Jane Doe
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 21 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Captain Picard
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score"> 44 </div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
+}
+
+Application.propTypes = {
+  title: React.PropTypes.string,
+};
+
+Application.defaultProps = {
+  title: "Scoreboard",
 }
 
 ReactDOM.render(<Application />, document.getElementById('container'));
